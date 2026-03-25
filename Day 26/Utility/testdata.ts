33a3c4edfdce2aeef377a67ee0bc8e15d9435ef1
mudit-101 <goyalmudit82@gmail.com>
2026-03-25 17:15:10 +0530

    task

---
+++ Day 26/Utility/testdata.ts
@@ -0,0 +1,20 @@
+export const TEST_DATA = {
+  customer: {
+    firstName:  'Rahul',
+    lastName:   'Sharma',
+    postCode:   '302033',
+  },
+
+  account: {
+    currency: 'Dollar',
+  },
+
+  transaction: {
+    depositAmount:  '5000',
+    withdrawAmount: '2000',
+  },
+
+  urls: {
+    base: 'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login',
+  },
+};
